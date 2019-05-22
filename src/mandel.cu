428b74096264e9b99efc250ad9bfd7c3ba2479a2
Daniel Godin <daniel.godin@epita.fr>
2019-05-23 00:06:16 +0200

    [GPU] First version of first kernel done

--- src/mandel.cu
+++ src/mandel.cu
@@ -1,28 +1,62 @@
 #include <stdio.h>
+#include <iostream>
+
+
+
 
 __global__
-void mandel_iter(int maxiter, int *d_res)
+void mandel_iter(int maxiter, int *d_res, int width, int height)
 {
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    d_res[i] = maxiter;
+    const unsigned int offset = blockIdx.x*blockDim.x + threadIdx.x;
+    int X = offset % width;
+    int Y = (offset-X)/width;
+
+    int idx = width * Y + X;
+
+    float x0 = ((float)X / width) * (3.5) - 2.5;
+    float y0 = ((float)Y /height) * (2) - 1;
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    int iter = 0;
+    float xtemp;
+    while((x *x + y *y <= 4.0f) && (iter < maxiter)){
+      xtemp = x * x - y * y + x0;
+      y = 2.0f * x * y + y0;
+      x = xtemp;
+      iter++;
+    }
+
+    d_res[idx] = iter;
 }
 
 
 int main(void)
 {
-    int N = 100
+    int HEIGHT = 1200;
+    int WIDTH = 800;
+
+    int N = WIDTH * HEIGHT;
     int *d_res;
     int *iter_res;
     iter_res = (int*)malloc(N*sizeof(int));
 
     cudaMalloc(&d_res, N*sizeof(int));
 
-    mandel_iter<<<(N+255)/256, 256>>>(N, d_res);
+   dim3 blockDims(512,1,1);
+   dim3 gridDims((unsigned int) ceil((double)(WIDTH*HEIGHT/blockDims.x)), 1, 1 );
+
+
+    mandel_iter<<<gridDims, blockDims>>>(N, d_res, WIDTH, HEIGHT);
 
     cudaMemcpy(iter_res, d_res, N*sizeof(int), cudaMemcpyDeviceToHost);
 
+    for (int i = 0; i < N; i++){
+      if (iter_res[i] != 0)
+        std::cout << iter_res[i] << std::endl;
+    }
 
-    
     cudaFree(d_res);
     free(iter_res);
 
