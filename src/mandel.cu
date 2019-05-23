6bdd8c568f6f2b7683c49c738cafef805ebd235f
Alexandre Lamure <alexandre.lamure@epita.fr>
2019-05-23 12:24:27 +0200

    EDT - add rest of the render code to mandel.cu

--- src/mandel.cu
+++ src/mandel.cu
@@ -39,7 +39,7 @@ rgba8_t heat_lut(float x)
 
 
 __global__
-void mandel_iter(int maxiter, int *d_res, int width, int height)
+void mandel_iter(int *iter_matrix, int width, int height, int n_iterations)
 {
     const unsigned int offset = blockIdx.x*blockDim.x + threadIdx.x;
     int X = offset % width;
@@ -55,44 +55,77 @@ void mandel_iter(int maxiter, int *d_res, int width, int height)
 
     int iter = 0;
     float xtemp;
-    while((x *x + y *y <= 4.0f) && (iter < maxiter)){
+    while((x *x + y *y <= 4.0f) && (iter < n_iterations)){
       xtemp = x * x - y * y + x0;
       y = 2.0f * x * y + y0;
       x = xtemp;
       iter++;
     }
 
-    d_res[idx] = iter;
+    iter_matrix[idx] = iter;
 }
 
 
-int main(void)
+void render_gpu(std::byte* buffer,
+                int width,
+                int height,
+                std::ptrdiff_t stride,
+                int n_iterations)
 {
-    int HEIGHT = 1200;
-    int WIDTH = 800;
+    int *histogram_cu;
+    int *histogram = new int[n_iterations];
+    for (int i = 0; i < n_iterations; ++i)
+        histogram[i] = 0;
 
-    int N = WIDTH * HEIGHT;
-    int *d_res;
-    int *iter_res;
-    iter_res = (int*)malloc(N*sizeof(int));
+    int N = width * height;
+    int *iter_matrix_cu;
+    int *iter_matrix = new int[N];
 
-    cudaMalloc(&d_res, N*sizeof(int));
+    cudaMalloc(&iter_matrix_cu, N*sizeof(int));
 
-   dim3 blockDims(512,1,1);
-   dim3 gridDims((unsigned int) ceil((double)(WIDTH*HEIGHT/blockDims.x)), 1, 1 );
+   dim3 nb_blocks(ceil((float(height)/32),(float(width)/1),1);
+   dim3 threads_per_block(32, 1, 1);
 
+    mandel_iter<<<nb_blocks, threads_per_block>>>(iter_matrix_cu,
+                                                  //histogram_cu,
+                                                  width, height,
+                                                  n_iterations);
 
-    mandel_iter<<<gridDims, blockDims>>>(N, d_res, WIDTH, HEIGHT);
+    cudaMemcpy(iter_matrix_cu, iter_matrix, N*sizeof(int), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(iter_res, d_res, N*sizeof(int), cudaMemcpyDeviceToHost);
-
-    for (int i = 0; i < N; i++){
-      if (iter_res[i] != 0)
-        std::cout << iter_res[i] << std::endl;
+    for (int i = 0; i < N; i++)
+    {
+        if (iter_matrix[i] != 0)
+            std::cout << iter_matrix[i] << std::endl;
     }
 
-    cudaFree(d_res);
+    cudaFree(iter_matrix_cu);
+
+    rgba8_t *hue = new rgba8_t[n_iterations + 1];
+    for (int i = 0; i < n_iterations + 1; ++i)
+        hue[i] = rgba8_t{0, 0, 0, 255};
+    float tmp = (float)histogram[0] / total;
+    hue[0] = heat_lut(tmp);
+    for (int i = 1; i < n_iterations; ++i)
+    {
+        tmp = tmp + ((float)histogram[i] / total);
+        hue[i] = heat_lut(tmp);
+    }
+ 
+    auto buffer_down = buffer + stride * (height - 1);
+    for (int Py = 0; Py < height / 2; ++Py)
+    {
+        rgba8_t* lineptr_top = reinterpret_cast<rgba8_t*>(buffer);
+        rgba8_t* lineptr_bottom = reinterpret_cast<rgba8_t*>(buffer_down);
+        for (int Px = 0; Px < width; ++Px)
+        {
+            lineptr_top[Px] = hue[iter_matrix[Py * width + Px]];
+            lineptr_bottom[Px] = hue[iter_matrix[Py * width + Px]];
+        }
+        buffer += stride;
+        buffer_down -= stride;
+    }
+
     free(iter_res);
 
-    return 0;
 }
